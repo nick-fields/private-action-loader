16a6676f3557fa7e66be2829086ad6b9cea68c79
Nick Fields <nickfields@invisionapp.com>
2019-12-08 09:39:34 -0500

    Added input validation to enforce required and default values

--- src/index.js
+++ src/index.js
@@ -9,11 +9,6 @@ const GITHUB_REPO = core.getInput('repo-name', { required: true })
 const WORKING_DIR = './.private-action'
 
 async function run () {
-
-  const inputs = Object.keys(process.env).filter(k=>k.startsWith('INPUT_'))
-
-  core.info(JSON.stringify(inputs, null, 2));
-
   const { repo, sha } = parseRepo()
 
   core.info('Masking token just in case')
@@ -41,11 +36,13 @@ async function run () {
   core.info('Reading action.yml')
   const actionFile = readFileSync(`${WORKING_DIR}/action.yml`, 'utf8')
   const action = parse(actionFile)
-
+  
   if (!(action && action.name && action.runs && action.runs.main)) {
     throw new Error('Malformed action.yml found')
   }
 
+  const inputs = setInputs(action)
+  
   core.endGroup('Cloning private action')
 
   core.info(`Starting private action ${action.name}`)
@@ -60,6 +57,28 @@ function parseRepo () {
     repo: repoArr[0],
     sha: repoArr[1]
   }
+}
+
+function setInputs(action){
+  if (!action.inputs) {
+    return;
+  }
+
+  for (const i of Object.keys(action.inputs)) {
+    const formattedInputName = `INPUT_${i.toUpperCase()}`;
+
+    if ((process.env[formattedInputName])||(!action.inputs[i].required && !action.inputs[i].default)) {
+      // the input was provided, or not provided but not required or w/ default
+      return;
+    } else if (action.inputs[i].required && !action.inputs[i].default) {
+      // input not provided, is required, and no default set
+      core.error(`Input ${i} required but not provided and no default is set`);
+    }
+
+    // input not provided so use the default
+    process.env[formattedInputName] = action.inputs[i].default
+  }
+
 }
 
 run().then(() => {
