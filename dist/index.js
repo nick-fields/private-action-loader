41c8b41f718b3710e7921f02f61e15f198df5e2e
Nick Fields <nickfields@invisionapp.com>
2020-02-12 22:39:48 -0500

    patch: another dependency bump

--- dist/index.js
+++ dist/index.js
@@ -7513,8 +7513,6 @@ const fixWinEPERM = (p, options, er, cb) => {
   assert(p)
   assert(options)
   assert(typeof cb === 'function')
-  if (er)
-    assert(er instanceof Error)
 
   options.chmod(p, 0o666, er2 => {
     if (er2)
@@ -7534,8 +7532,6 @@ const fixWinEPERM = (p, options, er, cb) => {
 const fixWinEPERMSync = (p, options, er) => {
   assert(p)
   assert(options)
-  if (er)
-    assert(er instanceof Error)
 
   try {
     options.chmodSync(p, 0o666)
@@ -7565,8 +7561,6 @@ const fixWinEPERMSync = (p, options, er) => {
 const rmdir = (p, options, originalEr, cb) => {
   assert(p)
   assert(options)
-  if (originalEr)
-    assert(originalEr instanceof Error)
   assert(typeof cb === 'function')
 
   // try to rmdir first, and only readdir on ENOTEMPTY or EEXIST (SunOS)
@@ -7672,8 +7666,6 @@ const rimrafSync = (p, options) => {
 const rmdirSync = (p, options, originalEr) => {
   assert(p)
   assert(options)
-  if (originalEr)
-    assert(originalEr instanceof Error)
 
   try {
     options.rmdirSync(p)
